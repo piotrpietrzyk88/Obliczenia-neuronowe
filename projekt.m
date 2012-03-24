49ded8427132a0dd83b60465c282328dc8d7a733
Piotr Pietrzyk <piotrpietrzyk88@gmail.com>
2012-03-24 10:22:41 +0100

    Pierwsze kroki!

---
+++ projekt.m
@@ -0,0 +1,51 @@
+clear all;
+close all;
+
+% NetDef        - struktura sieci
+% w1, w2        - wagi pocz¹tkowe
+% U           - u
+% Y             - y
+
+U = ones(1,100)*rand();
+x1 = zeros(1,length(U));
+x2 = zeros(1,length(U));
+Y = zeros(1,length(U));
+x1(1) = 0;
+x2(1) = U(1);
+
+for k = 1:length(U)
+    x1(k+1) = 0.5*x2(k) + 0.2*x1(k)*x2(k);
+    x2(k+1) = -0.3*x1(k) + 0.8*x2(k) + U(k);
+    Y(k) = x1(k) + (x2(k))^2;
+end
+
+
+
+%% settrain parameters 
+trparms = settrain; % Set trparms to default
+% maxiter       - maksymalna liczba iteracji
+% critmin       - kryterium stopu
+% eta           - krok
+% skip          - opusc tyle próbek
+trparms = settrain(trparms,'maxiter',1000,'critmin',1e-4, 'eta',0.02,'skip',5);
+
+w1 = [];
+w2 = [];
+NN = [ 3 3 1 ];
+NetDef=['HHHHHH';'H-----'];
+
+
+%% batbp parameters 
+% W1, W2        - wagi nauczonej sieci
+% crit_vector   - wektor b³êdów w kolejnych iteracjach
+% iter          - aktualna liczba iteracji
+[W1,W2,crit_vector,iter]=batbp(NetDef,w1,w2,U,Y,trparms);
+
+[W1,W2, NSSEvec,iter,lambda]=nnarx(NetDef,NN,W1,W2,trparms,Y,U);
+
+%% marq parameters
+
+% W1, W2        - wagi nauczonej sieci
+% crit_vector   - wektor b³êdów w kolejnych iteracjach
+% iter          - aktualna liczba iteracji
+[W1,W2,crit_vector,iter,lambda]=marq(NetDef,w1,w2,PHI,Y,trparms);
